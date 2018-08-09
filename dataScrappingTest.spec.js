58c65f226c587138735064611ed3ea789286e3d6
Abhishek Mazumder <abhishek.mazumder@crownbet.com.au>
2018-08-09 15:36:11 +1000

    Matching regex pattern

--- dataScrappingTest.spec.js
+++ dataScrappingTest.spec.js
@@ -4,5 +4,7 @@ test('Scrapping Test', async() => {
   console.log(`yellow dirty fellow`);
   let page = await global.__BROWSER__.newPage();
   await page.goto('http:\\www.google.com.au');
-  console.log(await page.$eval('*', el => el.innerText));
+  const innerText = await page.$eval('*', el => el.innerText);
+  const pattern = /gmail/i;
+  expect(innerText).toMatch(pattern);
 });
