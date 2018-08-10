48e7a02fb06c596d58853e503fd147d1caa6dcdf
Abhishek Mazumder <abhishek.mazumder@crownbet.com.au>
2018-08-10 13:55:42 +1000

    Code cleanup

--- test/dataScrappingTest.spec.js
+++ test/dataScrappingTest.spec.js
@@ -1,7 +1,6 @@
 jest.setTimeout(10000);
 
 test('Scrapping Test', async() => {
-  console.log(`yellow dirty fellow`);
   let page = await global.__BROWSER__.newPage();
   await page.goto('http:\\www.google.com.au');
   const innerText = await page.$eval('*', el => el.innerText);
